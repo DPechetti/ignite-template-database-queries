d7c20016157e48373920a3cec9908d9132758bd0
Donovan Pechetti <dovi_pf@hotmail.com>
2021-12-11 18:00:24 -0300

    fix findUsersByGameId

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -21,8 +21,6 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return this.repository.createQueryBuilder().relation(Game, "users").of(id).loadMany();
   }
 }
